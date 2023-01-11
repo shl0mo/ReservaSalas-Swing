af78fed6f034aff90ecf98cc83f1b825b3efbccd
shl0mo <salomao.dacostacruz@gmail.com>
2023-01-11 18:40:48 -0400

    feat(Interface): Login - 1

--- src/reserva/Login.java
+++ src/reserva/Login.java
@@ -11,9 +11,18 @@ public class Login extends JFrame {
 		this.frame.setLayout(null);
 		this.frame.setSize(400, 500);
 		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
 		Titulo.adicionaTitulo(this.frame);
 		
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		panel.setBounds(50, 70, 300, 300);
+		panel.setBackground(Color.BLUE);
+		JButton botao = new JButton("Botao");
+		botao.setSize(130, 40);
+		panel.add(botao);
+		panel.setVisible(true);
+		
+		this.frame.add(panel);
 	}
 	
 	public JFrame getFrame () {
