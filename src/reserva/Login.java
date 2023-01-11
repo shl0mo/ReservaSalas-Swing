8496941c027a98f23f3e44583c533fb4037b53ee
shl0mo <salomao.dacostacruz@gmail.com>
2023-01-11 19:16:32 -0400

    feat(interface): login - campos de usuario e senha

--- src/reserva/Login.java
+++ src/reserva/Login.java
@@ -13,14 +13,24 @@ public class Login extends JFrame {
 		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Titulo.adicionaTitulo(this.frame);
 		
-		
 		JPanel panel = new JPanel();
 		panel.setLayout(null);
 		panel.setBounds(50, 70, 300, 300);
 		panel.setBackground(Color.BLUE);
 		
-		JButton botao = new JButton("Botao");
-		botao.setSize(130, 40);
+		JTextField campo_usuario = new JTextField("Usuário");
+		campo_usuario.setBounds(70, 20, 100, 30);
+		panel.add(campo_usuario);
+		
+		
+		JPasswordField campo_senha = new JPasswordField("");
+		campo_senha.setBounds(0, 70, 100, 30);
+		//campo_senha.setHorizontalAlignment(JPasswordField.CENTER);
+		panel.add(campo_senha, BorderLayout.CENTER);
+		
+		
+		JButton botao = new JButton("Logar");
+		botao.setBounds(50, 130, 130, 40);
 		panel.add(botao);
 		panel.setVisible(true);
 		
